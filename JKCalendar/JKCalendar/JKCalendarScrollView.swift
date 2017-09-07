67b91fe48c9b59fd7f1401a0caf1837ef5ed3608
Anh Dung Pham <anh-dung.pham@jamitlabs.com>
2017-09-07 07:31:21 +0200

    Try to set another value for fixed scrolling

--- JKCalendar/JKCalendar/JKCalendarScrollView.swift
+++ JKCalendar/JKCalendar/JKCalendarScrollView.swift
@@ -108,8 +108,8 @@ public class JKCalendarScrollView: UIScrollView {
 extension JKCalendarScrollView: UIScrollViewDelegate {
     public func scrollViewDidScroll(_ scrollView: UIScrollView) {
 
-        if contentOffset.y >= -116.0 { // TODO: Get -116 programmatically
-            contentOffset.y = -116.0
+        if contentOffset.y >= -100.0 { // TODO: Get value programmatically
+            contentOffset.y = -100.0
         }
 
         var value = calendar.frame.height + contentOffset.y
